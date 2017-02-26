0f65d0e990071047f44a9391592ddee3e964fc0a
Robert Fitzpatrick <marcusaureliusalius@gmail.com>
2017-02-26 11:30:49 +0000

    Updating cmake and stubs for binary. Needs refactoring to implement 2x UDP servers instead of 2x UDP receivers and 2x UDP senders

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#define CATCH_CONFIG_MAIN
-
 #include <iostream>
 #include <list>
 #include <pcap.h>
@@ -9,7 +7,6 @@
 #include <chrono>
 #include <thread>
 
-#include "catch.hpp"
 #include "KMeans.hpp"
 #include "PacketAnalyser.hpp"
 #include "CUdpPair.hpp"
@@ -21,9 +18,32 @@
 
 using namespace std;
 
-/*
-int BinaryMain(int c, char * argv[])
+int main(int c, char * argv[])
 {
-  return 0;
+
+    JsonConfigFileReader configFileReader(configFilePath);
+
+	const std::uint16_t lLowServerPort = 10068;
+	const std::uint16_t lDiodePortHighToLow = 10070;
+	const std::uint16_t lDiodePortLowToHigh = 10071;		
+	
+	boost::asio::io_service lMainService;		
+	
+	CUdpPair UdpPair_HighToLow(std::string("127.0.0.1"), std::string("127.0.0.1"), lDiodePortHighToLow, lLowServerPort, lMainService);
+	CUdpPair UdpPair_LowToHigh(std::string("127.0.0.1"), std::string("127.0.0.1"), lDiodePortLowToHigh, 0, lMainService);	
+	
+    const std::uint32_t lMaxPacketsToObserve{1000};
+    const std::uint32_t lMaxTimeToObserve_s{3600};
+
+	CUdpProtocolLearner UdpProtocolLearner(UdpPair_HighToLow, 
+	                                       UdpPair_LowToHigh, 
+	                                       lMaxPacketsToObserve, 
+	                                       lMaxTimeToObserve_s);
+	
+	UdpProtocolLearner.mStartListening();	
+	
+	lMainService.run();
+    	
+    return 0;
 }
-*/
+
